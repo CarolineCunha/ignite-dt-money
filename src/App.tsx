d0d293f7071d9cb8d1a6f01c5d5bc93cb40ea8ad
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-03-23 07:31:38 -0300

    instalando styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
 export function App() {
   return (
     <div className="App">
-  
+      <Title>Hello World</Title>
     </div>
   );
 }
