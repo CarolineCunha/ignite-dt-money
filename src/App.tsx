65d5f9e84dd2f3c3cb65c95c3b7d953ce1639faf
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-03-24 07:31:51 -0300

    criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import styled from 'styled-components';
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-`
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
