46d240665db42dd19f6649f8b76712111cedbf6b
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-03-25 07:13:26 -0300

    componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import { Header } from "./components/Header/Index";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
