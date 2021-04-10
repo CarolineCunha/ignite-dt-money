e852745c80351d5ff5b75a0036b39e7a4824ce9a
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-04-10 07:10:01 -0300

    finalizando inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -14,7 +14,7 @@ type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 interface TransactionsContextData{
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 interface TransactionsProviderProps {
     children: ReactNode;
@@ -32,9 +32,18 @@ export function TransactionsProvider({ children }: TransactionsProviderProps){
         .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput){
+    async function createTransaction(transactionInput: TransactionInput){
         
-        api.post('/transactions', transaction)
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        })
+        const { transaction } = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction,
+        ]);
     }
 
     return (
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,15 +22,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
 
-    function handleCreateNewTransaction(event: FormEvent){
+    async function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
 
-        createTransaction({
+        await createTransaction({
             title,
             amount,
             category,
             type,
         })
+            setTitle('');
+            setAmount(0);
+            setCategory('');
+            setType('deposit');
+            onRequestClose();
     }
 
     return(
