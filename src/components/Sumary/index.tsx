6452bce34d79e15a82ba040a87a7e6147a9a14d9
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-04-10 11:09:38 -0300

    calculando resumo

--- src/components/Sumary/index.tsx
+++ src/components/Sumary/index.tsx
@@ -7,7 +7,24 @@ import React, { useContext } from 'react';
 
 export function Sumary(){
     const {transactions} = useContext(TransactionsContext);
-    console.log(transactions);
+    
+    const sumary = transactions.reduce((acc, transaction) => {
+
+        if(transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+
+        } else {
+            acc.withdraws -= transaction.amount;
+            acc.total -= transaction.amount;
+        }
+        return acc;
+       
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    });
     return(
       <Container> 
             <div>
@@ -15,21 +32,30 @@ export function Sumary(){
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>{new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(sumary.deposits)}</strong>
             </div>
             <div>
                 <header>
                     <p>Saidas</p>
                     <img src={outcomeImg} alt="Saidas" />
                 </header>
-                <strong>- R$500,00</strong>
+                <strong> {new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(sumary.withdraws)}</strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>{new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(sumary.total)}</strong>
             </div>
         </Container>
     )
